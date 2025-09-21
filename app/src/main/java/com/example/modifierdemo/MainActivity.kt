4f701dcbe8531caaedaea1c2208e2bcadea86b5a
SofiIlina <sofi0207@mail.ru>
2025-09-21 16:29:57 +0700

    Create modifier

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.modifierdemo
+package com.example.modifierdemo //объявляет пакет, которому принадлежит файл
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,36 +12,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : ComponentActivity() { //Activity, который является точкой входа в Android-приложение
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ModifierDemoTheme {
+        enableEdgeToEdge() // функция позволяет вашему контенту рисоваться под системными барами
+        setContent { //функция с описанием интерфейса
+            ModifierDemoTheme { // сгенерированная тема приложения
+                // Scaffold - макет для экрана
+                //innerPadding — автоматически рассчитанные отступы
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    DemoScreen(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+@Composable //сегмент пользовательского интерфейа
+fun DemoScreen(modifier: Modifier = Modifier) {
+    //функция принимает модификатор по умолчанию, что позволяет настраивать ее извне
+
+    val mymodifier = modifier
+        .border(width = 2.dp, color = Color.Black) //черная рамка в 2 пикселя
+        .padding(all = 10.dp) //отступы 10 px со всех сторон
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        "Hello Compose",
+        modifier = mymodifier,
+        fontSize = 40.sp, //размер 40 единиц
+        fontWeight = FontWeight.Bold //жирный текст
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true) //предосмотр внутри студии
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ModifierDemoTheme {
-        Greeting("Android")
+        DemoScreen()
     }
 }
+
+//При запуске приложения система создает MainActivity
+//Вызывается метод onCreate
+//setContent отрисовывает весь UI
+//Scaffold создает базовый макет экрана и рассчитывает отступы
+//Внутрь Scaffold помещается функция DemoScreen, которая и отображает текст "Hello Compose"
+//Функция DefaultPreview не участвует в работе реального приложения
